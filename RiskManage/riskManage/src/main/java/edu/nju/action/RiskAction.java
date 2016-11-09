0aeb6d9a0cc786f7045b2dfb030727dd2fac218b
lsySummer <864986781@qq.com>
2016-11-09 16:25:02 +0800

    新增部分内容

--- RiskManage/riskManage/src/main/java/edu/nju/action/RiskAction.java
+++ RiskManage/riskManage/src/main/java/edu/nju/action/RiskAction.java
@@ -1,8 +1,37 @@
 package edu.nju.action;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
-@Controller
-public class RiskAction {
+import edu.nju.model.RiskItem;
+import edu.nju.service.RiskService;
 
+@SuppressWarnings("serial")
+@Controller
+public class RiskAction extends BaseAction{
+	@Autowired
+	RiskService riskService;
+	
+	public String add(){
+		String content=request.getParameter("content");
+		String possibility=request.getParameter("possibility");
+		String influence=request.getParameter("influence");
+		String trig=request.getParameter("trig");
+		String submiter=request.getParameter("submiter");
+		String follower=request.getParameter("follower");
+		RiskItem r=new RiskItem();
+		r.setContent(content);
+		r.setPossibility(possibility);
+		r.setLevel(influence);
+		r.setRiskTrigger(trig);
+		r.setSubmitterId(Integer.parseInt(submiter));
+		r.setFollowerId(Integer.parseInt(follower));
+		boolean result=riskService.add(r);
+		if(result==true){
+			return "success";
+		}else{
+			return "error";
+		}
+	}
+	
 }
--- RiskManage/riskManage/src/main/java/edu/nju/dao/impl/RiskDaoImpl.java
+++ RiskManage/riskManage/src/main/java/edu/nju/dao/impl/RiskDaoImpl.java
@@ -1,5 +1,6 @@
 package edu.nju.dao.impl;
 
+import java.util.Date;
 import java.util.List;
 
 import org.hibernate.Session;
@@ -21,6 +22,13 @@ public class RiskDaoImpl implements RiskDao {
 	public boolean add(RiskItem risk) {
 		try {
 			baseDao.save(risk);
+			int id=risk.getId();
+			RiskState state=new RiskState();
+			state.setRid(id);
+			state.setCreateTime(new Date());
+			state.setDetail("建立风险条目");
+			state.setState("新建");
+			addState(state);
 			return true;
 		} catch (Exception e) {
 			e.printStackTrace();

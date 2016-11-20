0b6507fc67896f47bc236668b48bfcf54253c674
Lack of Stamina <lwpqq000@gmail.com>
2016-11-21 08:04:22 +0900

    ………………

--- src/main/java/edu/nju/LOGGER.java
+++ src/main/java/edu/nju/LOGGER.java
@@ -4,16 +4,20 @@ import org.apache.log4j.Logger;
 
 public class LOGGER {
 
-	private static Logger logger = Logger.getRootLogger();
+	private static Logger defaultLogger = Logger.getRootLogger();
 
 	private LOGGER() {
 	}
 
 	public static void log(Exception ex) {
-		logger.error("Exception: ", ex);
+		defaultLogger.error("Exception: ", ex);
 	}
 
 	public static void log(String message) {
-		logger.debug(message);
+		defaultLogger.debug(message);
+	}
+
+	public static void log(Object obj) {
+		defaultLogger.debug(obj);
 	}
 }
